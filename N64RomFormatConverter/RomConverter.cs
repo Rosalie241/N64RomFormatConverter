506c3348ed27554a5ae76f72da2263c4c3b7a4f4
Tim Wanders <tim241@mailbox.org>
2019-06-05 12:04:34 +0200

    Provide proper attribution

--- N64RomFormatConverter/RomConverter.cs
+++ N64RomFormatConverter/RomConverter.cs
@@ -1,16 +1,10 @@
-﻿/*
- N64RomFormatConverter - https://gitlab.com/tim241/N64RomFormatConverter
- Copyright (C) 2019 Tim Wanders <tim241@mailbox.org>
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- You should have received a copy of the GNU General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+﻿/* 
+ Copyright 2019 Tim Wanders <tim241@mailbox.org>
+ Copyright 2016 https://github.com/masl123 (no full name or email has been provided)
+ 
+ The following code is a derivative work of the code from the n64RomConverter project(https://github.com/masl123/n64RomConverter),
+ which is licensed LGPLv3. This code therefore is licensed under the terms 
+ of the GNU Public License, verison 3.
 */
 using System;
 using System.Security.Cryptography;
